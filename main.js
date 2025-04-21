36a591da7231ffd8e00ed06ac6c8dade7ffd4c8e
Sanjeev Kumar K S <sanjeevsiva80@gmail.com>
2025-04-21 23:14:25 +0530

    route structure

--- main.js
+++ main.js
@@ -8,6 +8,30 @@ app.get('/', (req, res) => {
   res.json({msg : "Hello World!"})
 })
 
+//CRUd functionality of movies
+
+
+//R - Read movies
+app.get('/movies', (req, res) => {
+
+})
+
+
+
+//C - for creating movies
+app.post('/movies', (req, res) => {
+  
+})
+
+//U - Update movies
+app.put('/movies/:id', (req, res) => {
+  
+})  
+
+//D - Delete movies
+app.delete('/movies/:id', (req, res) => {
+  
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
